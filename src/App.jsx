fe923fc861c92fa97c181a436d41fb8e7f8ad2ca
Sheikh Mohd Naved <navedkhn07@gmail.com>
2024-09-20 20:34:08 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,8 @@ function App() {
     <div id="main">
         <div id="card">
           <div id="part1"></div>
-            {/* <img src="https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"
-                 alt=""> */}
             <div id="part2">
-                <h1> Rafella Mendes</h1>
+                <h1> Mary Winchester</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, temporibus. Nemo quisquam voluptatem
                     aut tempora ullam enim perferendis. Doloribus, commodi?</p>
                 <h5>Russian Model</h5>
