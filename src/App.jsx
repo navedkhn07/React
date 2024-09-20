9573f62093e5e0bb8cea7fa7f35137b7a4f95e0a
Sheikh Mohd Naved <navedkhn07@gmail.com>
2024-09-20 19:19:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+    <div id="main">
+        <div id="card">
+          <div id="part1"></div>
+            {/* <img src="https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"
+                 alt=""> */}
+            <div id="part2">
+                <h1> Rafella Mendes</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, temporibus. Nemo quisquam voluptatem
+                    aut tempora ullam enim perferendis. Doloribus, commodi?</p>
+                <h5>Russian Model</h5>
+                <i class="ri-star-fill"></i><i class="ri-star-fill"></i><i class="ri-star-fill"></i><i
+                    class="ri-star-fill"></i><i class="ri-star-half-line"></i>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default App
